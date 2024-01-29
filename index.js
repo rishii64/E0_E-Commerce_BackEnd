cc8ed1b91340494cda7131fb4d7a6c35805480f1
rishii64 <saptarsismmitra@gmail.com>
2024-01-30 03:24:29 +0530

    updated

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const store = require('./Store/Store')
 const cors = require('cors')
 
 app.use(cors({
-    origin:"*"
+    origin: "*"
 }))
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     return res.send(store)
 })
-app.get('/products/:productData',(req,res)=>{
+app.get('/products/:productData', (req, res) => {
     let category = req.params.productData
-    let data = store.filter(data=> data.category === `${category}`)
+    let data = store.filter(data => data.category === `${category}`)
     return res.send(data)
 })
-app.get('/product/:category/:id',(req,res)=>{
-    const {id} = req.params
-    let data = store.filter(data=> data.id === Number(id))
+app.get('/product/:category/:id', (req, res) => {
+    const { id } = req.params
+    let data = store.filter(data => data.id === Number(id))
     return res.send(data)
 })
-app.listen(4000,(req,res)=>{
-    try{
+app.listen(4000, (req, res) => {
+    try {
         console.log('server is running fine');
     }
-    catch(err){
-        console.error('Error:',err)
+    catch (err) {
+        console.error('Error:', err)
     }
 })
